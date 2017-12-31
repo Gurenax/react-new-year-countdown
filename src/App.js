b8c12c93c60591c812b9232a72ed53ade967c66b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-31 09:46:21 +0800

    Add countdown timer function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import Countdown from './components/Countdown'
 import './App.css';
 
+import moment from 'moment'
+
+// const countdownTimer = () => {
+  
+
+// }
+
 class App extends Component {
   state = {
     countdownText: 'Hello'
   }
 
+
+  timer = () => {
+    const eventTime = moment('01-01-2018 00:00:00', 'DD-MM-YYYY HH:mm:ss').unix()
+    const currentTime = moment().unix()
+    const diffTime = eventTime - currentTime
+    let duration = moment.duration( diffTime * 1000, 'milliseconds' )
+    const interval = 1000
+
+    if( diffTime > 0 ) {
+
+      setInterval( () => {
+        duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds')
+        this.setState({ countdownText: duration.hours() + ":" + duration.minutes() + ":" + duration.seconds() })
+      }, interval)
+    }
+  }
+
+  
   render() {
     const { countdownText } = this.state
 
@@ -16,6 +41,10 @@ class App extends Component {
         <Countdown text={countdownText}/>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.timer()
   }
 }
 
