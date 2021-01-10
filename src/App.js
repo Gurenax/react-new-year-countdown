36c94f456112626f0ba229e868777eed9adf269f
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2021-01-11 09:30:14 +1100

    Refactor react code to functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Countdown from './components/Countdown'
 import './App.css'
 
@@ -16,67 +16,59 @@ const formatCountdownTextLabel = (digit, label, strict) => {
   else return formatTimerDigit(digit) + label
 }
 
-class App extends Component {
-  state = {
-    newYear: '2022',
-    countdownText: ''
-  }
+const App = () => {
+  const newYear = '2022';
+  const [countdownText, setCountdownText] = useState('');
 
-  timer = newYear => {
-    const eventTime = moment(
-      `01-01-${newYear} 00:00:00`,
-      'DD-MM-YYYY HH:mm:ss'
-    ).unix()
-    const currentTime = moment().unix()
-    const diffTime = eventTime - currentTime
-    let duration = moment.duration(diffTime * 1000, 'milliseconds')
-    const interval = 1000
-
-    if (diffTime > 0) {
-      setInterval(() => {
-        if (duration.asMilliseconds() > 0) {
-          duration = moment.duration(
-            duration.asMilliseconds() - interval,
-            'milliseconds'
-          )
-
-          // Format the months/days/hours/mins/secs with labels
-          const months = formatCountdownTextLabel(duration.months(), 'mo ', true)
-          const days = formatCountdownTextLabel(duration.days(), 'd ', true)
-          const hours = formatCountdownTextLabel(duration.hours(), 'h ', true)
-          const mins = formatCountdownTextLabel(duration.minutes(), 'm ', true)
-          const secs = formatCountdownTextLabel(duration.seconds(), 's ', false)
-
-          this.setState({ countdownText: months + days + hours + mins + secs })
-        } else {
-          this.setState({ countdownText: 'Happy New Year!' })
-        }
-      }, interval)
-    } else {
-      this.setState({ countdownText: 'Happy New Year!' })
+  useEffect(() => {
+    const timer = newYear => {
+      const eventTime = moment(
+        `01-01-${newYear} 00:00:00`,
+        'DD-MM-YYYY HH:mm:ss'
+      ).unix()
+      const currentTime = moment().unix()
+      const diffTime = eventTime - currentTime
+      let duration = moment.duration(diffTime * 1000, 'milliseconds')
+      const interval = 1000
+  
+      if (diffTime > 0) {
+        setInterval(() => {
+          if (duration.asMilliseconds() > 0) {
+            duration = moment.duration(
+              duration.asMilliseconds() - interval,
+              'milliseconds'
+            )
+  
+            // Format the months/days/hours/mins/secs with labels
+            const months = formatCountdownTextLabel(duration.months(), 'mo ', true)
+            const days = formatCountdownTextLabel(duration.days(), 'd ', true)
+            const hours = formatCountdownTextLabel(duration.hours(), 'h ', true)
+            const mins = formatCountdownTextLabel(duration.minutes(), 'm ', true)
+            const secs = formatCountdownTextLabel(duration.seconds(), 's ', false)
+  
+            setCountdownText(months + days + hours + mins + secs);
+          } else {
+            setCountdownText('Happy New Year!');
+          }
+        }, interval)
+      } else {
+        setCountdownText('Happy New Year!');
+      }
     }
-  }
 
-  render() {
-    const { newYear, countdownText } = this.state
-
-    return (
-      <div className="App">
-        <h1>New Year Countdown to {newYear}</h1>
-        <Countdown text={countdownText} />
-        <a href="https://github.com/Gurenax/react-new-year-countdown">
-          Fork me on Github
-        </a>
-      </div>
-    )
-  }
-
-  componentDidMount() {
-    // Get the new year from state
-    const { newYear } = this.state
     // Call the timer function
-    this.timer(newYear)
-  }
+    timer(newYear)
+  });
+
+  return (
+    <div className="App">
+      <h1>New Year Countdown to {newYear}</h1>
+      <Countdown text={countdownText} />
+      <a href="https://github.com/Gurenax/react-new-year-countdown">
+        Fork me on Github
+      </a>
+    </div>
+  )
 }
 
 export default App
